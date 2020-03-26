f15c6ba6ae90b6c6606d0981332901c5af133c47
ypraw <dyo.ndutz@gmail.com>
2020-03-26 23:20:32 +0700

    chore: register command subtract

--- commands.php
+++ commands.php
@@ -3,5 +3,6 @@
 return [
     // TODO : Add list of commands here
     Jakmall\Recruitment\Calculator\Commands\AddFunction::class,
+    Jakmall\Recruitment\Calculator\Commands\SubtractFunction::class,
 
 ];
