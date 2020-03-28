40e93909f59127732081e9fa89ab05970c4c2e87
ypraw <dyo.ndutz@gmail.com>
2020-03-28 22:51:41 +0700

    style: fix psr2 style

--- src/Commands/AddFunction.php
+++ src/Commands/AddFunction.php
@@ -26,9 +26,10 @@ class AddFunction extends Command
         echo $result. "\n";
     }
 
-    protected function calculate() {
+    protected function calculate()
+    {
         $number = $this->getInput();
-        if(count($number) > 0 and $this->numericalCheck($number)===true) {
+        if (count($number) > 0 and $this->numericalCheck($number)===true) {
             $description       = $this->generateCommand($number);
             $resultCalculation = $this->calculateAll($number);
             $finalResult = strval($description)." = ".strval($resultCalculation);
@@ -62,7 +63,7 @@ class AddFunction extends Command
     protected function calculateAll(array $numbers)
     {
         $result = null;
-        if(count($numbers) > 0) {
+        if (count($numbers) > 0) {
             $result = array_sum($numbers);
         }
         return $result;
@@ -74,10 +75,10 @@ class AddFunction extends Command
     protected function numericalCheck(array $numbers)
     {
         foreach ($numbers as $value) {
-            if(!is_numeric($value)){
-				return false;
+            if (!is_numeric($value)) {
+                return false;
             }
         }
-		return true;
+        return true;
     }
 }
